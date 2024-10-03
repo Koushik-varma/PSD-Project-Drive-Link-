0b8571ecf421cd4ea1b01e7b64555f601643090a
eswar <reddy.eswar2002>
2024-10-03 11:27:10 -0500

    profile

--- app/src/App.js
+++ app/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './Register';
 import Login from './Login';
+import Profile from './Profile'; // Import the Profile component
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/Profile" element={<Profile />} /> {/* Updated route to profile-management */}
         {/* Add more routes here if needed */}
       </Routes>
     </Router>
