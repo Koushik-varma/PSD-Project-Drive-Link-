82412ca6dc285f3c7f4493a7c9bbdf2356e231d6
SrinivasaVarmaP <psvarma610@gmail.com>
2024-10-14 12:44:34 -0500

    Update car-listing.php
    Added main content

--- car-listing.php
+++ car-listing.php
@@ -12,7 +12,7 @@ error_reporting(0);
 <meta name="viewport" content="width=device-width,initial-scale=1">
 <meta name="keywords" content="">
 <meta name="description" content="">
-<title>Car Rental Portal | Car Listing</title>
+<title>DriveLink | Car Listing</title>
 
 <!--Bootstrap -->
 <link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css">
@@ -55,3 +55,54 @@ error_reporting(0);
 <!--Header--> 
 <?php include('includes/header.php'); ?>
 <!-- /Header --> 
+
+<!-- Banners -->
+<section id="banner" class="banner-section">
+  <div class="container">
+    <div class="div_zindex">
+      <div class="row">
+        <div class="col-md-5 col-md-push-7">
+          <div class="banner_content">
+            <h1>Find the right car for you.</h1>
+            <p>We have more than a thousand cars for you to choose from.</p>
+            <a href="car-listing.php" class="btn">Explore Cars <span class="angle_arrow"><i class="fa fa-angle-right" aria-hidden="true"></i></span></a> 
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- /Banners --> 
+
+<!-- Listings Section -->
+<section class="listing-page">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-9 col-md-push-3">
+        <div class="result-sorting-wrapper">
+          <div class="sorting-count">
+            <p><span>1-6</span> of <span>50 Listings</span></p>
+          </div>
+        </div>
+
+        <!-- Listing -->
+        <div class="product-listing-m gray-bg">
+          <div class="product-listing-img"><img src="assets/images/car1.jpg" class="img-responsive" alt="Image" /> </div>
+          <div class="product-listing-content">
+            <h5><a href="car-details.php">Toyota Prius</a></h5>
+            <p class="list-price">$45 per day</p>
+            <ul>
+              <li><i class="fa fa-user" aria-hidden="true"></i> 5 seats</li>
+              <li><i class="fa fa-calendar" aria-hidden="true"></i> 2020 model</li>
+              <li><i class="fa fa-car" aria-hidden="true"></i> Automatic</li>
+            </ul>
+            <a href="car-details.php" class="btn">View Details <span class="angle_arrow"><i class="fa fa-angle-right" aria-hidden="true"></i></span></a> 
+          </div>
+        </div>
+        <!-- /Listing -->
+        
+      </div>
+    </div>
+  </div>
+</section>
+<!-- /Listings Section --> 
