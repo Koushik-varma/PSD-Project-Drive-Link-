bec7c438680ded5035dbc156ac4d46b97fe64529
SrinivasaVarmaP <psvarma610@gmail.com>
2024-10-16 18:26:32 -0500

    Update sql_file.sql
    Final Database file

--- sql_file.sql
+++ sql_file.sql
@@ -1,3 +1,10 @@
+-- Database: `carrental`/'DriveLink'
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 -- Table structure for table `tblcontactusinfo`
 
 CREATE TABLE `tblcontactusinfo` (
